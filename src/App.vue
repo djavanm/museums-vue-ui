345086947f41444360ca9b1c2369d191012ce599
Djavan Munroe <djavanm24@gmail.com>
2019-11-05 13:01:47 -0700

    Update App.vue
    Create initial styling and add new components for Exhibits and ExhibitMiniCard.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>hi, I'm an App</p>
+    <Exhibits v-bind:exhibits="exhibits" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Exhibits from './components/Exhibits';
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Exhibits
+  },
+  data() {
+    return {
+      exhibits: [],
+      next: '',
+      prev: '',
+      current: {}
+    }
+  },
+  created() {
+    const url = `https://api.harvardartmuseums.org/exhibition?venue=HAM&hasimage=1&apikey=${process.env.VUE_APP_API_KEY}`
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.exhibits = data.records;
+        this.next = data.info.next;
+        this.current = data.records[0];
+      });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
 </style>
