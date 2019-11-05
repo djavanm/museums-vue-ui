92cc95a2563a11ab75e0ab6b0aabc12177c83f9d
Djavan Munroe <djavanm24@gmail.com>
2019-11-05 14:19:28 -0700

    Create CurrentExhibit.vue
    Add CurrentExhibit which shows information on the selected component.

---
+++ src/components/CurrentExhibit.vue
@@ -0,0 +1,19 @@
+<template>
+  <article class="current-exhibit">
+    <h2>Current Exhibit </h2>
+    <img :src="exhibit.primaryimageurl" />
+  </article>
+</template>
+
+<script>
+export default {
+  name: "CurrentExhibit",
+  props: ["exhibit"]
+}
+</script>
+
+<style scoped>
+  .current-exhibit {
+    border: 2px solid yellow;
+  }
+</style>
